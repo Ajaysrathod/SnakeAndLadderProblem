cf73067a5ff6d01411f7ca3c7d18f5b99c5b8c5a
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-04-13 05:07:31 +0530

    [Ajay]-Position of player

---
+++ SnakeAndLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class SnakeAndLadder
+    {
+        //Created method position
+        public static void Position()
+        {
+            int position = 0;
+            Console.WriteLine("The position of player start with {0}", position);
+        }
+    }
+}
