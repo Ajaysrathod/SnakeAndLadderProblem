95da95165e6b87977f02bd20fa33990669098e69
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-04-13 05:18:25 +0530

    [Ajay]-Welcome to Die Rolled

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Welcome to Snake and Ladder program");
             //Taking user input
             Console.WriteLine("Please enter option given below");
-            Console.WriteLine("1:Position of Player");
+            Console.WriteLine("1:Position of Player\n2:Die rolled");
             int option = Convert.ToInt32(Console.ReadLine());
             //Switch case to call different methods
             switch (option)
@@ -15,7 +15,14 @@
                 case 1:
                     SnakeAndLadder.Position();
                     break;
+                case 2:
+                    SnakeAndLadder.DieRolled();
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
             }
         }
+
     }
 }
